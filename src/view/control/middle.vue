b2e21601343eb844e0ee4e37e76b7cd8c654e6bb
tiancong <tiancong@longshine.com>
2019-03-08 17:48:53 +0800

    暂存

--- src/view/control/middle.vue
+++ src/view/control/middle.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="list">
+  <div id="list">
     <div id='div1' class='divRec'>
       <board></board>
     </div>
@@ -12,7 +12,7 @@
     <div id='div4' class='divCard'>
       <news></news>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -27,69 +27,16 @@ export default {
     return {
 
     }
+  },
+  methods: {
   }
 }
 </script>
 
 <style lang="less" scoped>
-  section {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    user-select: none;
-    display: grid;
-    grid-gap: 15px;
-    grid-template-rows: repeat(2, 200px);
-    grid-template-columns: repeat(2, 1fr);
-    grid-auto-flow: row dense;
-    min-height: 100vh;
-  }
-
-  .divRec {
-      cursor: move;
-      padding: 35px 20px;
-      font-size: 20px;
-      background-color: #ff0000;
-
-      grid-area: span 1/span 2;
-  }
-
-  .divQuad {
-      cursor: move;
-      padding: 35px 20px;
-      font-size: 20px;
-      background-color: #fff333;
-
-      grid-area: span 1/span 1;
-  }
-
-  .divCard {
-      cursor: move;
-      padding: 35px 20px;
-      font-size: 20px;
-      background-color: #00ff00;
-
-      grid-area: span 2/span 1;
-  }
-
-  /* .custom-drag-ghost { */
-    /* The original cloned element must not take place up in the page and must not be visible */
-    /* position: absolute;
-    top: -99999px;
-    left: -99999px; */
-    /* Just for appearance */
-    /* background-color: #edb458;
-    border: 1px solid #e8871e;
-  } */
-
-  .hidden-drag-ghost {
-    opacity: 0;
-  }
-
-  .transition {
-    transition: all 2s ease-out .5s;
-    top: 0;
-  }
+#div1 {
+  width: 50%;
+  height: 50%;
+  background-color: #fff;
+}
 </style>
--- src/view/control/middle/board.vue
+++ src/view/control/middle/board.vue
@@ -1,13 +1,80 @@
 <template>
-  <div class="board">看板</div>
+  <div class="board">
+    <div class="title">跟踪看板</div>
+    <el-tabs v-model="activeName">
+      <el-tab-pane label="申报跟踪" name="first">
+      <el-form>
+        <el-form-item label="按单日期查询">
+          <el-date-picker
+            v-model="dates"
+            size="mini"
+            type="daterange"
+            align="right"
+            unlink-panels
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            :picker-options="pickerOptions">
+          </el-date-picker>
+
+        </el-form-item>
+      </el-form>
+      </el-tab-pane>
+      <el-tab-pane label="物流跟踪" name="second">配置管理</el-tab-pane>
+      <el-tab-pane label="费用跟踪" name="third">角色管理</el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      dates: '',
+      activeName: 'first',
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近一个月',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近三个月',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+            picker.$emit('pick', [start, end])
+          }
+        }]
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.board {
+  position: relative;
+  padding: 10px 20px;
+  .title {
+    position:absolute;
+    left: 20px;
+    top: 10;
+    line-height: 40px;
+    color: @font-color-main;
+    font-weight: bold;
+  }
+}
 </style>
