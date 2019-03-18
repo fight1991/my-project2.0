c51440fc0bddd3976bc81c17c9fbe08a9495cd3f
tiancong <tiancong@longshine.com>
2019-03-18 13:58:48 +0800

    暂存

--- src/view/common/slider.vue
+++ src/view/common/slider.vue
@@ -3,30 +3,30 @@
     <div :class="{'collapse-btn':true,'btn-rotate':true}" @click='collapseClick()' v-if="isRotate"></div>
     <div :class="{'collapse-btn':true}" @click='collapseClick()' v-else></div>
     <el-menu
-      default-active="2"
+      :default-active="$route.path" router
       :collapse='$store.state.collapse'>
       <template v-for="item in menus">
         <!-- 一级菜单 -->
-        <el-submenu :index="item.path" :key="item.path" v-if="item.children && item.children.length>0">
+        <el-submenu :index="item.path" :key="item.path" v-if="item.children && !item.hidden" v-permissions="item.permissions">
           <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>{{item.title}}</span>
+            <i :class=" 'custom-icons-menu '+item.icon"></i>
+            <span slot="title">{{item.name}}</span>
           </template>
           <!-- 二级菜单 -->
           <template v-for="child in item.children">
-            <el-submenu :index="child.path" :key="child.path" v-if="item.children && item.children.length>0">
+            <el-submenu :index="child.path" :key="child.path" v-if="child.children&& !child.hidden" v-permissions="child.permissions">
               <template slot="title"><i :class="child.icon"></i>{{child.name}}</template>
-              <el-menu-item v-for="child2 in child.children" :index="child2.path" :key="child2.path" v-if="!child2.hidden">
+              <el-menu-item v-for="child2 in child.children" :index="child2.path" :key="child2.path" v-if="!child2.hidden" v-permissions="child2.permissions">
                 <span slot="title">{{child2.name}}</span>
               </el-menu-item>
             </el-submenu>
-            <el-menu-item style='padding-left: 60px' v-if="!child.children" :index="child.path" :key="child.path">
+            <el-menu-item v-if="!child.children && !child.hidden" :index="child.path" :key="child.path"  v-permissions="child.permissions">
               <span slot="title">{{child.name}}</span>
             </el-menu-item>
           </template>
         </el-submenu>
-        <el-menu-item  v-if="!item.children" :index="item.path" :key="'child'+item.path">
-          <i :class="'custom-icons-menu'"></i>
+        <el-menu-item  v-if="!item.children && !item.hidden" :index="item.path" :key="'child'+item.path" v-permissions="item.permissions">
+          <i :class=" 'custom-icons-menu '+item.icon"></i>
           <span slot="title">{{item.name}}</span>
         </el-menu-item>
       </template>
@@ -38,45 +38,45 @@ export default {
   data () {
     return {
       menus: [
-        {
-          title: '厂商管理',
-          icon: require('../../assets/img/icon-aside/admin.png'),
-          activeIcon: require('../../assets/img/icon-aside/admin.png'),
-          path: '172.0.0.1:9003#1',
-          children: [
-            {
-              title: '管理一',
-              path: '172.0.0.1:9003#1-2'
-            },
-            {
-              title: '管理二',
-              path: '172.0.0.1:9003#1-3'
-            }
-          ]
-        },
-        {
-          title: '进出口模板',
-          icon: require('../../assets/img/icon-aside/ie-default.png'),
-          activeIcon: require('../../assets/img/icon-aside/ie-default.png'),
-          path: '172.0.0.1:9003#2',
-          children: [
-            {
-              title: '管理一',
-              path: '172.0.0.1:9003#2-2'
-            },
-            {
-              title: '管理二',
-              path: '172.0.0.1:9003#2-3'
-            }
-          ]
-        },
-        {
-          title: '接单',
-          icon: require('../../assets/img/icon-aside/receipt.png'),
-          activeIcon: require('../../assets/img/icon-aside/receipt.png'),
-          path: '172.0.0.1:9003#3',
-          children: []
-        }
+        // {
+        //   title: '厂商管理',
+        //   icon: require('../../assets/img/icon-aside/admin.png'),
+        //   activeIcon: require('../../assets/img/icon-aside/admin.png'),
+        //   path: '172.0.0.1:9003#1',
+        //   children: [
+        //     {
+        //       title: '管理一',
+        //       path: '172.0.0.1:9003#1-2'
+        //     },
+        //     {
+        //       title: '管理二',
+        //       path: '172.0.0.1:9003#1-3'
+        //     }
+        //   ]
+        // },
+        // {
+        //   title: '进出口模板',
+        //   icon: require('../../assets/img/icon-aside/ie-default.png'),
+        //   activeIcon: require('../../assets/img/icon-aside/ie-default.png'),
+        //   path: '172.0.0.1:9003#2',
+        //   children: [
+        //     {
+        //       title: '管理一',
+        //       path: '172.0.0.1:9003#2-2'
+        //     },
+        //     {
+        //       title: '管理二',
+        //       path: '172.0.0.1:9003#2-3'
+        //     }
+        //   ]
+        // },
+        // {
+        //   title: '接单',
+        //   icon: require('../../assets/img/icon-aside/receipt.png'),
+        //   activeIcon: require('../../assets/img/icon-aside/receipt.png'),
+        //   path: '172.0.0.1:9003#3',
+        //   children: []
+        // }
       ], // 菜单数据
       isRotate: true,
       active: '', // 活动菜单
