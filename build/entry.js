77a92955fdec0e41a1eae7771effcfd1cad4b7ec
wangyz <516271227@qq.com>
2019-04-03 11:13:59 +0800

    系统优化

--- build/entry.js
+++ build/entry.js
@@ -7,7 +7,13 @@ module.exports = {
     app: './src/main.js',
     components: './src/components/index.js',
     common: './src/view/common/index.js',
-    error: './src/view/error/index.js'
+    error: './src/view/error/index.js',
+    www: './src/view/www/index.js',
+    control: './src/view/control/index.js',
+    aircraft: './src/view/pages/aircraft/router.js',
+    dataCenter: './src/view/pages/dataCenter/router.js',
+    eImport: './src/view/pages/eImport/router.js',
+    reportCenter: './src/view/pages/reportCenter/router.js'
   },
   build: {
     vendor1: ['vue'],
@@ -17,6 +23,12 @@ module.exports = {
     app: ['babel-polyfill', './src/main.js'],
     components: ['babel-polyfill', './src/components/index.js'],
     common: ['babel-polyfill', './src/view/common/index.js'],
-    error: ['babel-polyfill', './src/view/error/index.js']
+    error: ['babel-polyfill', './src/view/error/index.js'],
+    www: ['babel-polyfill', './src/view/www/index.js'],
+    control: ['babel-polyfill', './src/view/control/index.js'],
+    aircraft: ['babel-polyfill', './src/view/pages/aircraft/router.js'],
+    dataCenter: ['babel-polyfill', './src/view/pages/dataCenter/router.js'],
+    eImport: ['babel-polyfill', './src/view/pages/eImport/router.js'],
+    reportCenter: ['babel-polyfill', './src/view/pages/reportCenter/router.js']
   }
 }
