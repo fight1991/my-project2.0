1dcdf9ff93f1e4be0d84d7e0a4ebbddea09b8c47
tiancong <tiancong@longshine.com>
2019-04-12 11:32:05 +0800

    暂存

--- src/view/pages/sea/router.js
+++ src/view/pages/sea/router.js
@@ -1,5 +1,5 @@
 // import baoheJobs from './baoheJobs/index' // 报核业务
-// import beianJobs from './beianJobs/index' // 备案业务
+import beianJobs from './beianJobs/index' // 备案业务
 // import czhanJobs from './czhanJobs/index' // 场站业务
 import lareJobs from './lareJobs/index' // 申报业务
 const MENU = [
@@ -13,7 +13,7 @@ const MENU = [
       title: '首页'
     }
   },
-  // beianJobs.MENU,
+  beianJobs.MENU,
   lareJobs.MENU
   // baoheJobs.MENU,
   // czhanJobs.MENU
