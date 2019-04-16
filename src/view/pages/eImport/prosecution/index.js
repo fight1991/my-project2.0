5243de3b403a3c8f135b7133d9142efdc1562d38
guyu <guyu@longshine.com>
2019-04-16 17:31:44 +0800

    逻辑监控权限

--- src/view/pages/eImport/prosecution/index.js
+++ src/view/pages/eImport/prosecution/index.js
@@ -3,7 +3,7 @@ const MENU = {
   path: '/eImport/prosecution',
   name: 'prosecution',
   icon: 'icons-menu-setting',
-  permissions: 'SAASOM0900000000',
+  permissions: 'CCBA20207000000',
   component: resolve => require(['../../../common/abstract.vue'], resolve),
   meta: {
     title: '逻辑检控设置'
@@ -13,7 +13,7 @@ const MENU = {
       path: '/eImport/prosecution/priceRange',
       name: 'priceRange',
       icon: 'icons-menu-price',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20207010000',
       hidden: false,
       meta: {
         component: resolve => require(['./priceRange.vue'], resolve),
@@ -23,7 +23,7 @@ const MENU = {
       path: '/eImport/prosecution/ship',
       name: 'ship',
       icon: 'icons-menu-price',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20207020000',
       hidden: false,
       meta: {
         component: resolve => require(['./ship.vue'], resolve),
