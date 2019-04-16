5c8e36185c1e09b60abe622545c226ec3c634fc9
wangyz <516271227@qq.com>
2019-04-16 13:30:36 +0800

    分页组件优化

---
+++ src/common/mixin/pagination.js
@@ -0,0 +1,13 @@
+const pagination = {
+  data () {
+    return {
+      pagination: {
+        currentPage: 1, // 当前页
+        pageSize: 10, // 每页数据条数
+        total: 0 // 总条数
+      }
+    }
+  }
+}
+
+export { pagination }
