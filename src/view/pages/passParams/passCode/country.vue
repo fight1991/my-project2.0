074ca9b057e4f7f41596a6a0015c9c5094a91f87
guyu <guyu@longshine.com>
2019-04-18 11:20:59 +0800

    通关参数

---
+++ src/view/pages/passParams/passCode/country.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class='sys-main'>
+    <!-- 查询条件 -->
+    <div class="query-header">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-input size="mini" placeholder="国别代码/国别名称" maxlength="50"></el-input>
+        </el-col>
+        <el-col :span="10">
+          <el-button size="mini" type="primary" @click="search">查询</el-button>
+          <!-- <el-button size="mini">重置</el-button> -->
+        </el-col>
+      </el-row>
+    </div>
+    <!-- 查询条件 end -->
+    <!-- 主显示框 -->
+    <div class='query-body'>
+      <el-table class='sys-table-table' :data="countryList" border highlight-current-row size="mini">
+        <el-table-column label="序号" width="130">
+          <!-- <template slot-scope="scope">
+            <div class='sys-td-c'>{{(pages.pageIndex-1)*pages.pageSize+(scope.$index+1)}}</div>
+          </template> -->
+        </el-table-column>
+        <el-table-column label="国别代码" min-width="150">
+          <template slot-scope="scope">
+            <div class='sys-td-c'>{{scope.row.title}}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="国别名称" min-width="150">
+          <template slot-scope="scope">
+            <div class='sys-td-c'>{{scope.row.title}}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="英文名称" min-width="150">
+          <template slot-scope="scope">
+            <div class='sys-td-c'>{{scope.row.title}}</div>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页-->
+      <el-row class='sys-page-list'>
+        <el-col :span="24" align="right">
+          <page-box @change="pageList"></page-box>
+        </el-col>
+      </el-row>
+      <!-- 分页 end -->
+    </div>
+    <!-- 主显示框 end -->
+  </section>
+</template>
+<script>
+export default {
+  name: 'country',
+  data () {
+    return {
+      countryForm: {
+
+      },
+      countryList: []
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    // 查询
+    search () {
+
+    },
+    // 获取表格
+    pageList () {
+
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+
+</style>
--- src/view/pages/passParams/passCode/index.js
+++ src/view/pages/passParams/passCode/index.js
@@ -3,7 +3,7 @@ const MENU = {
   path: '/passParams/passCode',
   name: 'passParams-passCode',
   icon: 'icons-menu-passCode',
-  permissions: 'SAASOM0900000000',
+  permissions: 'CCBA20202000000',
   component: resolve => require(['../../../common/abstract.vue'], resolve),
   meta: {
     title: '通关代码'
@@ -12,160 +12,160 @@ const MENU = {
     {
       path: '/passParams/customs/list',
       name: 'customs',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./customs.vue'], resolve),
         title: '关区'
       }
     },
     {
       path: '/passParams/country/list',
       name: 'country',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./country.vue'], resolve),
         title: '国别（地区）'
       }
     },
     {
       path: '/passParams/money/list',
       name: 'money',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./money.vue'], resolve),
         title: '币制'
       }
     },
     {
       path: '/passParams/unit/list',
       name: 'unit',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./unit.vue'], resolve),
         title: '计量单位'
       }
     },
     {
       path: '/passParams/controlMode/list',
       name: 'controlMode',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./controlMode.vue'], resolve),
         title: '监管方式'
       }
     },
     {
       path: '/passParams/transport/list',
       name: 'transport',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./transport.vue'], resolve),
         title: '运输方式'
       }
     },
     {
       path: '/passParams/redemp/list',
       name: 'redemp',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./redemp.vue'], resolve),
         title: '征免性质'
       }
     },
     {
       path: '/passParams/port/list',
       name: 'port',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./port.vue'], resolve),
         title: '港口'
       }
     },
     {
       path: '/passParams/pakageKinds/list',
       name: 'pakageKinds',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./pakageKinds.vue'], resolve),
         title: '包装种类'
       }
     },
     {
       path: '/passParams/bargain/list',
       name: 'bargain',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./bargain.vue'], resolve),
         title: '成交方式'
       }
     },
     {
       path: '/passParams/regulate/list',
       name: 'regulate',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./regulate.vue'], resolve),
         title: '监管证件'
       }
     },
     {
       path: '/passParams/license/list',
       name: 'license',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./license.vue'], resolve),
         title: '随附单据'
       }
     },
     {
       path: '/passParams/tradePro/list',
       name: 'tradePro',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./tradePro.vue'], resolve),
         title: '优惠贸易协定'
       }
     },
     {
       path: '/passParams/inLandArea/list',
       name: 'inLandArea',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./inLandArea.vue'], resolve),
         title: '国内地区'
       }
     },
     {
       path: '/passParams/taxFree/list',
       name: 'taxFree',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./taxFree.vue'], resolve),
         title: '征减免税方式'
       }
     },
     {
       path: '/passParams/inLandPort/list',
       name: 'inLandPort',
-      permissions: 'SAASOM0504000000',
+      permissions: 'CCBA20202010000',
       hidden: false,
       meta: {
-        component: resolve => require(['./index.vue'], resolve),
+        component: resolve => require(['./inLandPort.vue'], resolve),
         title: '国内口岸'
       }
     }
