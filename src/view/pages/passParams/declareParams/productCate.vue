eab645841de4284798253db257b9756f39521b81
guyu <guyu@longshine.com>
2019-04-19 18:42:17 +0800

    参数

---
+++ src/view/pages/passParams/declareParams/productCate.vue
@@ -0,0 +1,104 @@
+<template>
+  <section class='sys-main'>
+    <!-- 查询条件 -->
+    <div class="query-header">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-input size="mini" placeholder="类别/目录名称/简介" maxlength="50"></el-input>
+        </el-col>
+        <el-col :span="10">
+          <el-button size="mini" type="primary" @click="search">查询</el-button>
+          <!-- <el-button size="mini">重置</el-button> -->
+        </el-col>
+      </el-row>
+    </div>
+    <!-- 查询条件 end -->
+    <!-- 主显示框 -->
+    <div class='query-body'>
+      <el-table class='sys-table-table' :data="productList" border highlight-current-row size="mini">
+        <el-table-column label="序号" width="130">
+          <!-- <template slot-scope="scope">
+            <div class='sys-td-c'>{{(pages.pageIndex-1)*pages.pageSize+(scope.$index+1)}}</div>
+          </template> -->
+        </el-table-column>
+        <el-table-column label="类别" min-width="150">
+          <template slot-scope="scope">
+            <div class='sys-td-c'>{{scope.row.title}}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="目录名称" min-width="150">
+          <template slot-scope="scope">
+            <div class='sys-td-c'>{{scope.row.title}}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="简介" min-width="150">
+          <template slot-scope="scope">
+            <div class='sys-td-c'>{{scope.row.title}}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="注释" min-width="100">
+          <template slot-scope="scope">
+            <div class='param-td-c'><a href="javascript:void(0)" class="list-icon-look border-0" title="查看" @click="toDetail(scope.row)"><i class='dec-i'></i></a></div>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页-->
+      <el-row class='sys-page-list'>
+        <el-col :span="24" align="right">
+          <page-box @change="pageList"></page-box>
+        </el-col>
+      </el-row>
+      <!-- 分页 end -->
+    </div>
+    <!-- 主显示框 end -->
+  </section>
+</template>
+<script>
+export default {
+  name: 'productCate',
+  data () {
+    return {
+      productForm: {
+
+      },
+      productList: [{}]
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    // 查询
+    search () {
+
+    },
+    // 获取表格
+    pageList () {
+
+    },
+    // 跳转详情
+    toDetail () {
+
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+.param-td-c{
+  text-align: center;
+}
+.list-icon-look{
+  i{
+    display: inline-block;
+    width: 14px;
+    height: 14px;
+    margin-right: 6px;
+    vertical-align: middle;
+    margin-top: -2px;
+    background: url('../../../../assets/img/icon/icon-look.png') no-repeat;
+  }
+  &:hover i,&:focus i{
+    background: url('../../../../assets/img/icon/icon-lookH.png') no-repeat;
+  }
+}
+</style>
