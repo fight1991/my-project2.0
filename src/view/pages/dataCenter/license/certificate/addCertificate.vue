0234b10f4ab2c07c737409812eeac2c9c41f6317
caoyingying <546404923@qq.com>
2019-05-09 10:19:43 +0800

    bug修复

--- src/view/pages/dataCenter/license/certificate/addCertificate.vue
+++ src/view/pages/dataCenter/license/certificate/addCertificate.vue
@@ -255,8 +255,12 @@ export default {
       }
       let restaurants = this.corpListOptions
       let results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants
-      // 调用 callback 返回建议列表的数据
-      cb(results.slice(0, 10))
+      if (results.length === 0) {
+        this.addForm.ownerCodeScc = ''
+      } else {
+        // 调用 callback 返回建议列表的数据
+        cb(results.slice(0, 10))
+      }
     },
     createFilter (queryString) {
       return (restaurant) => {
