045ed36bcb92e5771b2b49992b91ea836782bd50
tiancong <tiancong@longshine.com>
2019-05-13 14:30:30 +0800

    添加labelWidth全局混入

--- src/common/mixin/labelWidth.js
+++ src/common/mixin/labelWidth.js
@@ -3,12 +3,12 @@ const labelWidth = {
   data () {
     return {
       labelWidth: {
-        seven: '110px', // label 7个字的宽度 超过6个字折行,配合css使用
-        six: '', // label 6个字的宽度
-        five: '',
-        four: '',
-        three: '',
-        two: ''
+        seven: '107px', // label 7个字的宽度 超过6个字折行,配合css使用
+        six: '93px', // label 6个字的宽度
+        five: '79px',
+        four: '65px',
+        three: '51px',
+        two: '37px'
       }
     }
   }
