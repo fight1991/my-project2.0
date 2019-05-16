3171f7116780c776273ef1df16d8b5634b74379b
guyu <guyu@longshine.com>
2019-05-16 16:59:33 +0800

    修改

--- src/view/pages/eImport/decRegister/index.vue
+++ src/view/pages/eImport/decRegister/index.vue
@@ -2,15 +2,32 @@
     <iframe-view :url='url'></iframe-view>
 </template>
 <script>
+import base64 from '@/common/base64'
 import pathList from '@/config/pathList'
 export default {
   name: 'index',
   data () {
     return {
-      url: pathList.eImport['decRegister'] + '?sysId=CCBA&tabId=' + this.$route.name + '&token=' + encodeURIComponent(window.localStorage.getItem('token'))
+      url: ''
     }
   },
   mounted () {
+    let params = ''
+    if (this.$route.params.sysData || this.$route.query.sysData) {
+      // 说明从从子系统打开此页面
+      let datas = []
+      if (this.$route.params.sysData) {
+        datas = JSON.parse(base64.decode(this.$route.params.sysData))
+      } else if (this.$route.query.sysData) {
+        datas = JSON.parse(base64.decode(this.$route.query.sysData))
+      }
+      let data = JSON.parse(datas.params)
+      for (let item in data) {
+        params += `&${item}=${data[item]}`
+      }
+      params += `&tabId=${datas.tabId}`
+    }
+    this.url = pathList.eImport['decRegister'] + '?sysId=CCBA' + params + '&tabId=' + this.$route.name + '&token=' + encodeURIComponent(window.localStorage.getItem('token'))
   }
 }
 </script>
