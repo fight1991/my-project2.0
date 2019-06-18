712be973fd806d252be6294db6855e3c52219251
caoyingying <546404923@qq.com>
2019-06-18 14:45:07 +0800

    人才招聘暂存

--- src/view/www/components/talent.vue
+++ src/view/www/components/talent.vue
@@ -1,46 +1,42 @@
 <template>
   <div class="talent">
-    <div class="title">
-      <p class="big">人才招聘</p>
-      <p class="light">TALENT RECRUITMENT</p>
+    <!-- 头图 -->
+    <div class="header-img"></div>
+    <!-- 介绍 -->
+    <div class="banner">
+      <el-row>
+        <el-col :span="18" :offset="3">
+          <el-row :gutter="20">
+          <el-col :span="8">
+            <div class="banner-bgimg" v-html="jobIntroduce.first"></div>
+          </el-col>
+          <el-col :span="8">
+            <div class="banner-bgimg" v-html="jobIntroduce.second"></div>
+          </el-col>
+          <el-col :span="8">
+            <div class="banner-bgimg" v-html="jobIntroduce.third"></div>
+          </el-col>
+          </el-row>
+        </el-col>
+      </el-row>
     </div>
-    <div class="mainer items-info">
-      <div class="item-detail" v-for="item in jobs" :key="item.aid">
-        <div class="zp-title">
-          <div class="link">
-            <div class="name">{{item.link.name}}</div>
-            <div class="tel">{{item.link.tel}}</div>
-          </div>
-          <div class="email">{{item.link.email}}</div>
-          <div class="company">{{item.link.company}}</div>
-        </div>
-        <div class="zp-info" v-for="item1 in item.zp_info" :key="item1.pid">
-          <div class="job-title">{{item1.jobTitle}}</div>
-          <div class="self-info">
-            <div class="money marg-r" v-html="item1.wage"><span class="text-light">月薪:</span><span class="pad-l">{{item1.wage}}</span></div>
-            <div class="culture marg-r" v-html="item1.culture"><span class="text-light">学历:</span><span class="pad-l">{{item1.culture}}</span></div>
-            <div class="place" v-html="item1.place"><span class="text-light">地点:</span><span class="pad-l">{{item1.place}}</span></div>
-          </div>
-          <div class="duty" v-for="item2 in item1.detail" :key="item2.id">
-            <div class="duty-title">{{item2.title}}</div>
-            <p v-for="(item3,index) in item2.info" :key="index" v-html="item3"></p>
-          </div>
-        </div>
-      </div>
+    <div class="title">
+      <p class="big">诚聘人才</p>
+      <p class="light">RECRUITMENT TALENTS</p>
     </div>
   </div>
 </template>
 
 <script>
-import {jobs} from '@/config/www'
+import {jobIntroduce} from '@/config/www'
 export default {
   data () {
     return {
-      jobs: jobs
+      jobIntroduce: {}
     }
   },
   created () {
-
+    this.jobIntroduce = jobIntroduce
   },
   methods: {
 
@@ -49,85 +45,99 @@ export default {
 </script>
 
 <style lang="less" scoped>
- .talent {
-   padding: 40px 0;
-   color: #333;
- }
- .zp-title {
-   padding: 15px;
-   height:100px;
-   background:url("../../../assets/www-img/images/talent-head.png") no-repeat;
-   .email,.company {
-     margin: 10px 0;
-     color: #808080;
-   }
-   .link {
-     display: flex;
-     height: 40px;
-     line-height: 40px;
-     .name {
-       font-size: 20px;
-       font-weight: bold;
-       color: #333;
-     }
-     .tel {
-       font-size: 16px;
-       font-weight: bold;
-       line-height: 46px;
-       text-indent: 8px;
-       padding-right: 26px;
-       background: url("../../../assets/www-img/images/tel.png") no-repeat;
-       background-position: right 15px;
-     }
-   }
- }
- .zp-info {
-   padding: 15px;
- }
- .job-title {
-   font-size: 16px;
-   font-weight: bold;
-   color: #333;
-   padding: 10px 0;
- }
- .item-detail {
-   padding: 15px;
-   border: 1px solid #ccc;
-   margin-bottom: 50px;
-   &:first-child {
-     margin-top: 40px;
-   }
- }
- .self-info {
-   font-size: 14px;
-   display: flex;
-   padding: 10px 0;
-   .marg-r {
-     margin-right: 60px;
-   }
-   .money>.pad-l{
-     color: #FEA12E;
-   }
-   .text-light {
-     color: #808080;
-   }
- }
- .pad-l {
-     padding-left: 10px;
-   }
-  .company {
-    padding-bottom: 10px;
-    .text-light {
-     color: #808080;
-   }
+.talent {
+  color: #333;
+  .header-img {
+    height: 380px;
+    background: url('../../../assets/www-img/images/banner_img.png') no-repeat center;
+    background-size: cover;
+    box-sizing: border-box;
   }
-  .duty {
-    font-size: 12px;
-    .duty-title {
-      padding: 5px 0;
-    }
-    p {
-      margin: 8px 0;
+  .banner {
+    margin: 50px;
+    .banner-bgimg {
+      width: 320px;
+      height: 217px;
+      background: url('../../../assets/www-img/images/wz_bg.png') no-repeat center;
+      background-size: cover;
     }
   }
+}
+//  .zp-title {
+//    padding: 15px;
+//    height:100px;
+//    background:url("../../../assets/www-img/images/talent-head.png") no-repeat;
+//    .email,.company {
+//      margin: 10px 0;
+//      color: #808080;
+//    }
+//    .link {
+//      display: flex;
+//      height: 40px;
+//      line-height: 40px;
+//      .name {
+//        font-size: 20px;
+//        font-weight: bold;
+//        color: #333;
+//      }
+//      .tel {
+//        font-size: 16px;
+//        font-weight: bold;
+//        line-height: 46px;
+//        text-indent: 8px;
+//        padding-right: 26px;
+//        background: url("../../../assets/www-img/images/tel.png") no-repeat;
+//        background-position: right 15px;
+//      }
+//    }
+//  }
+//  .zp-info {
+//    padding: 15px;
+//  }
+//  .job-title {
+//    font-size: 16px;
+//    font-weight: bold;
+//    color: #333;
+//    padding: 10px 0;
+//  }
+//  .item-detail {
+//    padding: 15px;
+//    border: 1px solid #ccc;
+//    margin-bottom: 50px;
+//    &:first-child {
+//      margin-top: 40px;
+//    }
+//  }
+//  .self-info {
+//    font-size: 14px;
+//    display: flex;
+//    padding: 10px 0;
+//    .marg-r {
+//      margin-right: 60px;
+//    }
+//    .money>.pad-l{
+//      color: #FEA12E;
+//    }
+//    .text-light {
+//      color: #808080;
+//    }
+//  }
+//  .pad-l {
+//      padding-left: 10px;
+//    }
+//   .company {
+//     padding-bottom: 10px;
+//     .text-light {
+//      color: #808080;
+//    }
+//   }
+//   .duty {
+//     font-size: 12px;
+//     .duty-title {
+//       padding: 5px 0;
+//     }
+//     p {
+//       margin: 8px 0;
+//     }
+//   }
 </style>
