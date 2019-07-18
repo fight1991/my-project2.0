7a5be71a9becac8df6850151fd092507d7582546
tiancong <tiancong@longshine.com>
2019-07-18 14:14:55 +0800

    物流费用-去除税率可清空

--- src/view/pages/finance/costManage/costList.vue
+++ src/view/pages/finance/costManage/costList.vue
@@ -21,7 +21,7 @@
           </el-col>
           <el-col :span="6">
             <el-form-item label="税率">
-              <el-select v-model="QueryForm.feeRate" size="mini" clearable  style="width:100%;">
+              <el-select v-model="QueryForm.feeRate" size="mini" clearable style="width:100%;">
                 <el-option key="1" :label="'0%'" :value="0"></el-option>
                 <el-option key="2" :label="'6%'" :value="6"></el-option>
                 <el-option key="3" :label="'11%'" :value="11"></el-option>
@@ -117,7 +117,7 @@
           <el-row>
             <el-col :span="14" :offset="5">
               <el-form-item label="税率">
-                <el-select v-model="addFees.feeRate" size="mini" clearable  style="width:100%;">
+                <el-select v-model="addFees.feeRate" size="mini" style="width:100%;">
                   <el-option key="1" :label="'0%'" :value="0"></el-option>
                   <el-option key="2" :label="'6%'" :value="6"></el-option>
                   <el-option key="3" :label="'11%'" :value="11"></el-option>
