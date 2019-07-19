f675b4f49c535a677c353e6931063df337532bff
tiancong <tiancong@longshine.com>
2019-07-19 17:53:21 +0800

    物流费用-台账编辑暂存

--- src/view/pages/finance/expense/expenseDetail.vue
+++ src/view/pages/finance/expense/expenseDetail.vue
@@ -29,30 +29,32 @@
         <el-button size="mini" class="list-btns list-icon-add"><i></i>新增</el-button>
       </el-row>
       <div class='query-table'>
-        <el-table class='sys-table-table' :cell-class-name="getCellStyle" align="left" :data="billReceivableBodyVO.billReceivableBodyVOList" border>
+        <el-table class='sys-table-table' :cell-class-name="optionsType==='edit' && getCellStyle" align="left" :data="billReceivableBodyVO.billReceivableBodyVOList" border>
           <el-table-column prop="serialNo" label="序号" width="50" align="center">
           </el-table-column>
           <el-table-column prop="feeOptionName" label="费用名称">
             <template slot-scope="scope">
-              <div class="table-select">
+              <div class="table-select" v-if="optionsType === 'edit'">
                 <el-select size="mini" placeholder="请选择费用名称" clearable  v-model="scope.row.feeOptionName" style="width:100%;">
                   <el-option v-for="item in optionsList"
                     :key="item.feePid" :label="item.feeOptionName" :value="item.feeOptionName">
                   </el-option>
                 </el-select>
               </div>
+              <div class="cell-div" v-else>{{scope.row.feeOptionName || '-'}}</div>
             </template>
           </el-table-column>
           <el-table-column prop="feePrice" label="计费单价" align="right">
             <template slot-scope="scope">
-              <div class="table-select">
+              <div class="table-select" v-if="optionsType === 'edit'">
                 <el-input v-model="scope.row.feePrice"></el-input>
               </div>
+              <div class="cell-div" v-else>{{scope.row.feePrice || '-'}}</div>
             </template>
           </el-table-column>
           <el-table-column prop="unit" width="120" label="计量单位">
             <template slot-scope="scope">
-              <div class="table-select">
+              <div class="table-select" v-if="optionsType === 'edit'">
                 <el-select  v-model="scope.row.unit" placeholder="计量单位"
                   filterable remote default-first-option
                   @focus="tipsFill('unitList','SAAS_UNIT')"
@@ -66,11 +68,12 @@
                   </el-option>
                 </el-select>
               </div>
+              <div class="cell-div" v-else>{{scope.row.unit || '-'}}</div>
             </template>
           </el-table-column>
           <el-table-column prop="curr" width="120" label="币制" align="center">
             <template slot-scope="scope">
-              <div class="table-select">
+              <div class="table-select" v-if="optionsType === 'edit'">
                 <el-select  v-model="scope.row.curr" placeholder="币制"
                   filterable remote default-first-option
                   @focus="tipsFill('currList','SAAS_CURR')"
@@ -84,36 +87,58 @@
                   </el-option>
                 </el-select>
               </div>
+              <div class="cell-div" v-else>{{scope.row.curr || '-'}}</div>     
             </template>
           </el-table-column>
           <el-table-column prop="num" width="100" label="数量" align="center">
             <template slot-scope="scope">
-              {{scope.row.num  || '-'}}
+              <div class="table-select" v-if="optionsType === 'edit'">
+                <el-input v-model="scope.row.num"></el-input>
+              </div>
+              <div class="cell-div" v-else>{{scope.row.num || '-'}}</div>
             </template>
           </el-table-column>
           <el-table-column prop="feeRate" width="100" label="税率" align="center">
             <template slot-scope="scope">
-              <div class="table-select">
+              <div class="table-select" v-if="optionsType === 'edit'">
                 <el-select size="mini" placeholder="税率" style="width:100%;">
-                <el-option key="0" :label="'0%'" :value="0"></el-option>
-                <el-option key="6" :label="'6%'" :value="6"></el-option>
-                <el-option key="11" :label="'11%'" :value="11"></el-option>
-              </el-select>
+                  <el-option key="0" :label="'0%'" :value="0"></el-option>
+                  <el-option key="6" :label="'6%'" :value="6"></el-option>
+                  <el-option key="11" :label="'11%'" :value="11"></el-option>
+                </el-select>
               </div>
+              <div class="cell-div" v-else>{{(scope.row.feeRate && scope.row.feeRate + '%') || '-'}}</div>
             </template>
           </el-table-column>
           <el-table-column prop="taxPrice" width="80" label="含税总价" align="center">
             <template slot-scope="scope">
-              {{scope.row.taxPrice && (scope.row.taxPrice + '%') || '-'}}
+              <div class="cell-div">{{scope.row.taxPrice && (scope.row.taxPrice + '%') || '-'}}</div>
             </template>
           </el-table-column>
           <el-table-column prop="settleCompanyName" min-width="120" label="结算企业" align="center">
+            <template slot-scope="scope">
+              <div class="table-select" v-if="optionsType === 'edit'">
+                <el-input v-model="scope.row.settleCompanyName"></el-input>
+              </div>
+              <div class="cell-div" v-else>{{scope.row.settleCompanyName || '-'}}</div>
+            </template>
           </el-table-column>
           <el-table-column prop="billType" width="60" label="类型" align="center">
+            <template slot-scope="scope">
+              <div class="cell-div">
+                {{scope.row.billType === '0' ? '自动登账' : scope.row.billType === '1' ? '手动登账' : '-'}}
+              </div>
+            </template>
           </el-table-column>
-          <el-table-column prop="billType" min-width="120" label="使用报价" align="center">
+          <el-table-column prop="itemName" min-width="120" label="使用报价" align="center">
+            <template slot-scope="scope">
+              <div class="cell-div">{{scope.row.itemName || '-'}}</div>
+            </template>
           </el-table-column>
-          <el-table-column prop="billType" width="80" label="操作人" align="center">
+          <el-table-column prop="createUserId" width="80" label="操作人" align="center">
+            <template slot-scope="scope">
+              <div class="cell-div">{{scope.row.createUserId || '-'}}</div>
+            </template>
           </el-table-column>
         </el-table>
       </div>
@@ -135,6 +160,7 @@ import commonParam from '@/common/commonParam'
 export default {
   data () {
     return {
+      optionsType: 'look', // 记录当前操作类型
       billPayableBodyVO: { // 应付
         billPayableBodyVOList: [], // 下拉列表
         billQuotationRespVOs: [] // 表格数据
@@ -158,8 +184,9 @@ export default {
     }
   },
   created () {
-    let id = this.$route.query.expenseBillId
-    id && this.getBillDetail(id)
+    let {type, expenseBillId} = this.$route.query
+    expenseBillId && this.getBillDetail(expenseBillId)
+    this.optionsType = type
     this.getOptionList()
     this.getCommonParam()
   },
@@ -280,5 +307,9 @@ export default {
   }
   .table-btn {
     padding-bottom: 15px;
+  }
+  .cell-div {
+    padding: 5px 12px;
+    line-height: 30px;
   }
 </style>
