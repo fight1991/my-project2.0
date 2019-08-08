e03a9c015ce922419b2901187f418bf322f130d6
caoyingying <caoyingying@longshine.com>
2019-08-08 14:33:19 +0800

    返回按钮取消

--- src/view/pages/passParams/declareParams/taxRuleInfo.vue
+++ src/view/pages/passParams/declareParams/taxRuleInfo.vue
@@ -1,11 +1,11 @@
 <template>
   <section class='sys-main'>
     <!-- 返回按钮 -->
-    <el-row style="margin-bottom: 15px;">
+    <!-- <el-row style="margin-bottom: 15px;">
       <el-col :span='18' :xs='24'>
         <span @click="$router.push({name:'taxRule'})" class="sys-back-btn"><i class="back-btn"></i>返回</span>
       </el-col>
-    </el-row>
+    </el-row> -->
     <!-- 返回按钮 end-->
     <el-row class="params-info">
       <el-row class="params-border-panel">
