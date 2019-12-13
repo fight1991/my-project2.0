fbd1028878129cc29fb3ca7fb0c87cbbf2b7e45b
heying <heying@longshine.com>
2019-12-13 17:31:26 +0800

    样式添加

--- src/config/config.js
+++ src/config/config.js
@@ -10,9 +10,10 @@ export default {
     WWW: 'https://www.51baoguan.cn:8091/login/index?flag=1', // 旧版官网切换地址
     FINCE: 'https://test.5itrade.cn/ccba2', // 跳转至物流费用系统
     EMS: 'https://test.5itrade.cn/ems' // 跳转至金二
-    // COMMON: 'http://172.21.206.31:8072', // 公共登录、注册等跳转地址
-    // FILE: 'http://172.21.206.31:8072/zuul', // 文件上传
-    // API: 'http://172.21.206.31:9999', // api接口服务器地址
+    // COMMON: 'http://172.21.101.36:8072', // 公共登录、注册等跳转地址
+    // FILE: 'http://172.21.101.36:8072/zuul', // 文件上传
+    // API: 'http://172.21.101.36:9999', // api接口服务器地址
+    // API: 'http://172.21.101.36:9999', // api接口服务器地址
     // WWW: 'https://www.51baoguan.cn:8091/login/index?flag=1' // 旧版官网切换地址
   },
   // 测试、准生产、生产
